e2b0a482b385798e0b5f82f68c277dba23bd8783
Nitin <santynitin7@gmail.com>
2021-04-14 03:29:04 +0530

    this is my covid project 1 commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,58 @@
-import logo from './logo.svg';
+import React, { useEffect, useState } from 'react';
 import './App.css';
+import{MenuItem,FormControl,Select} from "@material-ui/core";
 
 function App() {
+    const [countries , setcountries] = useState([]); //inital state 
+    const [country, setcountry] = useState("World-wide"); 
+
+    useEffect(() => { // wait to load the data then fire 
+      const getCountriesData = async () => {
+        await fetch("https://disease.sh/v3/covid-19/countries?yesterday=true")
+        .then((response) => response.json())
+        .then((data) => {
+          const countries = data.map((country) => (
+            { 
+              name:country.country, //full names of countries 
+              value:country.countryInfo.iso2, //short name of countries
+            }));
+
+          setcountries(countries);
+        })
+      };
+      getCountriesData();  
+      }, []);
+        
+      const onCountryChange =  (event) => {
+        const countryCode = event.target.value;
+        // console.log("yoooo  coutry code", countryCode);
+        setcountry(countryCode);
+      }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+    <div className= "app_header">
+          <h1>Covid 19 Tracker</h1>
+          <FormControl className="app_dropdown">
+          <Select variant = "outlined" onChange={onCountryChange} value={country}>
+          {countries.map(country => (
+            <MenuItem value = {country.value}>{country.name}</MenuItem>
+          ))}
+          </Select>
+          </FormControl>
+    </div>
+
+      <div className="stats"> 
+        {/* {Infobox} */}
+        {/* {Infobox} */}
+        {/* {Infobox} */}
+        </div>
+
+    {/* {map} */}
+
+    {/* {table all the coutry} */}
+    {/* {graphs of cases} */}
+
     </div>
   );
 }
---
+++ src/Infobox.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Infobox() {
+    return (
+        <div>
+            
+        </div>
+    )
+}
+
+export default Infobox
